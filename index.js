a095e991e3c71d2a475e7ec6401d1557117d0c27
hibamalhis <hibamalhiss@gmail.com>
2020-11-20 21:49:03 +0200

    add style to chat app

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require("express")
-const path = require("path")
-const http = require("http")
-const socketio = require("socket.io")
-const { generateMessage } = require('./utils/message.js')
+express = require("express")
+path = require("path")
+http = require("http")
+socketio = require("socket.io")
 
 const app = express()
 const server = http.createServer(app)
@@ -10,12 +9,12 @@ const io = socketio(server)
 
 io.on("connection", (socket) => {
     // emit to the newly connected client
-    socket.emit('message', generateMessage("welcome to chat app!"))
+    socket.emit('message', 'welcome to chat app!')
     //emit to all connected client except the current one
     socket.broadcast.emit('message', 'a new user has joined!')
-    socket.on("clientMessage", (message, callback) => {
+    socket.on("clientMessage", (msg, callback) => {
         //emit to all connected clients
-        io.emit("message", generateMessage(message))
+        io.emit("message", msg)
         callback("ack!")
     })
 
